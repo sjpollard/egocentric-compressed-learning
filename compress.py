0b0ffb898116e9921ee8a8533e70f49c35adcc26
Sam Pollard <40839788+sjpollard@users.noreply.github.com>
2023-01-27 17:55:27 +0000

    Tensor dimensions as (Channels, Frames, Height, Width).

--- compress.py
+++ compress.py
@@ -10,35 +10,20 @@ def random_gaussian_matrix(shape):
 
     return M
 
+def compressed_features(X, phi_matrices, theta_matrices, modes):
+    Y = t.multi_mode_dot(X, phi_matrices, modes)
+    return t.multi_mode_dot(Y, theta_matrices, modes)
 
 def main():
 
-    """ X = np.array([[[1, 2, 3],
-                   [4, 5, 6],
-                   [7, 8, 9]],
-                  [[1, 2, 3],
-                   [4, 5, 6],
-                   [7, 8, 9]],
-                  [[1, 2, 3],
-                   [4, 5, 6],
-                   [7, 8, 9]]]) """
+    X = np.random.default_rng().integers(0, 255, (3, 10, 5, 5))
 
-    X = np.random.default_rng().integers(0, 255, (3, 50, 50))
+    M1 = random_gaussian_matrix((1, 3))
+    M2 = random_gaussian_matrix((5, 10))
+    M3 = random_gaussian_matrix((3, 5))
+    M4 = random_gaussian_matrix((3, 5))
 
-    print(X)
-    
-    M1 = random_gaussian_matrix((3, 3))
-    M2 = random_gaussian_matrix((25, 50))
-    M3 = random_gaussian_matrix((25, 50))
-
-    Y = t.multi_mode_dot(X, [M1, M2, M3], [0, 1, 2])
-
-    print(Y)
-
-    X_hat = t.multi_mode_dot(Y, [M1.T, M2.T, M3.T], [0, 1, 2])
-
-    print(X_hat)
-
+    X_hat = compressed_features(X, [M1, M2, M3, M4], [M1.T, M2.T, M3.T, M4.T], [0, 1, 2, 3])
 
 if __name__ == "__main__":
     main()
