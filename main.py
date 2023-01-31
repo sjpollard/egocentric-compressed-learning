27844d0c694edd7b1619299f89b0401e002628d4
Sam Pollard <40839788+sjpollard@users.noreply.github.com>
2023-01-31 18:35:02 +0000

    Code can now pull in sequences of frames using training set as a reference dataframe.

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import compress
 import numpy as np
 import tensorly as tl
+import pandas as pd
 from PIL import Image
 
 tl.set_backend('numpy')
 
+def get_frames(annotation):
+    participant_id = annotation[0]
+    video_id = annotation[1]
+    start_frame = annotation[2]
+    stop_frame = annotation[3]
+    zero = '0'
+    for i in range(start_frame, stop_frame + 1):
+        frame_id = f'{(10 - len(str(i))) * zero}{str(i)}'
+        frame = Image.open(f'EPIC-KITCHENS/{participant_id}/rgb_frames/{video_id}/frame_{frame_id}.jpg')
+
 def main():
-    W1 = compress.random_gaussian_matrix((1, 3))
-    image = Image.open('EPIC-KITCHENS/P01/rgb_frames/P01_01/frame_0000000001.jpg')
-    frame = np.asarray(image)
-    print(compress.compress_tensor(frame, [W1], [2]).shape)
+    train_annotations = pd.read_csv('annotations\EPIC_100_train.csv')
+    for annotation in train_annotations.loc[0:328, ['participant_id', 'video_id', 'start_frame', 'stop_frame', 'verb', 'noun']].values:
+        print(annotation)
+        get_frames(annotation)
+    
 
 
 if __name__ == '__main__':
