72f60cdabe76c4a73214f86a0a97ed2293c55d7e
Sam Pollard <40839788+sjpollard@users.noreply.github.com>
2023-02-02 16:32:28 +0000

    loader.py now pulls in frames and annotations.

--- loader.py
+++ loader.py
@@ -1,34 +1,43 @@
 import numpy as np
+import pandas as pd
 import tensorly as tl
 from PIL import Image
 
 tl.set_backend('numpy')
 
 
-def get_annotation_frames(annotation):
-    participant_id = annotation[0]
-    video_id = annotation[1]
-    start_frame = annotation[2]
-    stop_frame = annotation[3]
-    zero = '0'
-    i = map(str, range(start_frame, stop_frame + 1))
-    file_names = list(map(lambda x: f'{(10 - len(x)) * zero}{x}', i))
-    frames = list(map(lambda x: np.asarray(Image.open(
-        f'EPIC-KITCHENS/{participant_id}/rgb_frames/{video_id}/frame_{x}.jpg')), file_names))
-    return tl.tensor(frames), (annotation[4], annotation[5])
+class Loader:
+    def __init__(self, dataset_path, annotations_path):
+        self.dataset_path = dataset_path
+        self.train_annotations = pd.read_csv(
+            f'{annotations_path}/EPIC_100_train.csv')
+        self.val_annotations = pd.read_csv(
+            f'{annotations_path}/EPIC_100_validation.csv')
+        self.test_annotations = pd.read_csv(
+            f'{annotations_path}/EPIC_100_test_timestamps.csv')
 
+    def get_annotation_snippets(self, annotation, num_segments):
+        participant_id = annotation[0]
+        video_id = annotation[1]
+        start_frame = annotation[2]
+        stop_frame = annotation[3]
+        zero = '0'
+        segments = np.array_split(
+            np.arange(start_frame, stop_frame + 1), num_segments)
+        snippets = list(
+            map(lambda x: str(np.random.default_rng().choice(x)), segments))
+        file_names = list(
+            map(lambda x: f'{(10 - len(x)) * zero}{x}', snippets))
+        frames = list(map(lambda x: np.asarray(Image.open(
+            f'{self.dataset_path}/{participant_id}/rgb_frames/{video_id}/frame_{x}.jpg')), file_names))
+        return tl.tensor(frames)
 
-def get_annotation_snippets(annotation, num_segments):
-    participant_id = annotation[0]
-    video_id = annotation[1]
-    start_frame = annotation[2]
-    stop_frame = annotation[3]
-    zero = '0'
-    segments = np.array_split(
-        np.arange(start_frame, stop_frame + 1), num_segments)
-    snippets = list(
-        map(lambda x: str(np.random.default_rng().choice(x)), segments))
-    file_names = list(map(lambda x: f'{(10 - len(x)) * zero}{x}', snippets))
-    frames = list(map(lambda x: np.asarray(Image.open(
-        f'EPIC-KITCHENS/{participant_id}/rgb_frames/{video_id}/frame_{x}.jpg')), file_names))
-    return tl.tensor(frames), (annotation[4], annotation[5])
+    # TODO Change this to read in entire training set when I have it on hand.
+    def get_train(self, num_segments):
+        annotations = self.train_annotations.loc[0:328, ['participant_id',
+                                                         'video_id', 'start_frame', 'stop_frame']].values
+        train_X = np.array(
+            list(map(lambda x: self.get_annotation_snippets(x, num_segments), annotations)))
+        train_Y = self.train_annotations.loc[0:328, [
+            'verb', 'noun']].to_numpy()
+        return train_X, train_Y
--- main.py
+++ main.py
@@ -9,14 +9,10 @@ tl.set_backend('numpy')
 
 
 def main():
-    measurements = [25, 45, 1]
-    train_annotations = pd.read_csv('annotations\EPIC_100_train.csv')
-    for annotation in train_annotations.loc[0:0, ['participant_id', 'video_id', 'start_frame', 'stop_frame', 'verb', 'noun']].values:
-        X, Y = loader.get_annotation_snippets(annotation, 8)
-        W1 = compress.random_gaussian_matrix((measurements[2], X.shape[3]))
-        W2 = compress.random_gaussian_matrix((measurements[1], X.shape[2]))
-        W3 = compress.random_gaussian_matrix((measurements[0], X.shape[1]))
-        print(compress.compress_tensor(X, [W1, W2, W3], [3, 2, 1]))
+    load = loader.Loader('C:/Users/SAM/EPIC-KITCHENS',
+                         'C:/Users/SAM/Documents/GitHub/epic-kitchens-100-annotations')
+    train_X, train_Y = load.get_train(8)
+    print(train_X.shape)
 
 
 if __name__ == '__main__':
