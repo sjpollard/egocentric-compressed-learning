6afcd6a53cf4b2c67e41edc914e0623a23366e06
sjpollard <samjosephpollard@gmail.com>
2023-02-03 13:23:43 +0000

    Added Linux filepaths.

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ tl.set_backend('numpy')
 
 
 def main():
-    load = loader.Loader('C:/Users/SAM/EPIC-KITCHENS',
-                         'C:/Users/SAM/Documents/GitHub/epic-kitchens-100-annotations')
-    train_X, train_Y = load.get_train(8)
+    """ windows_load = loader.Loader('C:/Users/SAM/EPIC-KITCHENS',
+                         'C:/Users/SAM/Documents/GitHub/epic-kitchens-100-annotations') """
+    linux_load = loader.Loader('/home/hiraeth/EPIC-KITCHENS',
+                         '/home/hiraeth/Github/epic-kitchens-100-annotations')
+    train_X, train_Y = linux_load.get_train(8)
     print(train_X.shape)
 
 
