9bbf5de22cdff0aba78f612df9e0fc17ec1c199a
Sam Pollard <40839788+sjpollard@users.noreply.github.com>
2023-02-06 15:56:30 +0000

    Using PyTorch tensors instead of NumPy. Can now save tensors to file.

--- loader.py
+++ loader.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
 import tensorly as tl
+import torch
+from torchvision import transforms
 from PIL import Image
 
-tl.set_backend('numpy')
+tl.set_backend('pytorch')
 
 
 class Loader:
-    def __init__(self, dataset_path, annotations_path):
+    def __init__(self, dataset_path, annotations_path, data_path):
         self.dataset_path = dataset_path
         self.train_annotations = pd.read_csv(
             f'{annotations_path}/EPIC_100_train.csv')
@@ -15,8 +17,26 @@ class Loader:
             f'{annotations_path}/EPIC_100_validation.csv')
         self.test_annotations = pd.read_csv(
             f'{annotations_path}/EPIC_100_test_timestamps.csv')
+        self.data_path = data_path
 
     def get_annotation_snippets(self, annotation, num_segments):
+        participant_id = annotation[0]
+        video_id = annotation[1]
+        start_frame = annotation[2]
+        stop_frame = annotation[3]
+        zero = '0'
+        transform = transforms.Compose([transforms.PILToTensor()])
+        segments = np.array_split(
+            np.arange(start_frame, stop_frame + 1), num_segments)
+        snippets = list(
+            map(lambda x: str(np.random.default_rng().choice(x)), segments))
+        file_names = list(
+            map(lambda x: f'{(10 - len(x)) * zero}{x}', snippets))
+        frames = torch.stack(list(map(lambda x: transform(Image.open(
+            f'{self.dataset_path}/{participant_id}/rgb_frames/{video_id}/frame_{x}.jpg')), file_names)))
+        return frames
+
+    def get_annotation_snippets2(self, annotation, num_segments):
         participant_id = annotation[0]
         video_id = annotation[1]
         start_frame = annotation[2]
@@ -30,14 +50,20 @@ class Loader:
             map(lambda x: f'{(10 - len(x)) * zero}{x}', snippets))
         frames = list(map(lambda x: np.asarray(Image.open(
             f'{self.dataset_path}/{participant_id}/rgb_frames/{video_id}/frame_{x}.jpg')), file_names))
-        return tl.tensor(frames)
+        return frames
 
     # TODO Change this to read in entire training set when I have it on hand.
     def get_train(self, num_segments):
         annotations = self.train_annotations.loc[0:328, ['participant_id',
                                                          'video_id', 'start_frame', 'stop_frame']].values
-        train_X = np.array(
+        train_X = torch.stack(
             list(map(lambda x: self.get_annotation_snippets(x, num_segments), annotations)))
-        train_Y = self.train_annotations.loc[0:328, [
-            'verb', 'noun']].to_numpy()
+        train_Y = torch.tensor(self.train_annotations.loc[0:328, [
+            'verb_class', 'noun_class']].values)
         return train_X, train_Y
+
+    def save_to_pt(self, filename, tensor):
+        torch.save(tensor, f'{self.data_path}/{filename}')
+
+    def load_from_pt(self, filename):
+        return torch.load(f'{self.data_path}/{filename}')
--- main.py
+++ main.py
@@ -5,16 +5,23 @@ import tensorly as tl
 import compress
 import loader
 
-tl.set_backend('numpy')
+tl.set_backend('pytorch')
+
+
+def preprocess_epic(loader):
+    train_X, train_Y = loader.get_train(8)
+    loader.save_to_pt('train_X.pt', train_X)
+    loader.save_to_pt('train_Y.pt', train_Y)
 
 
 def main():
-    """ windows_load = loader.Loader('C:/Users/SAM/EPIC-KITCHENS',
-                         'C:/Users/SAM/Documents/GitHub/epic-kitchens-100-annotations') """
-    linux_load = loader.Loader('/home/hiraeth/EPIC-KITCHENS',
-                         '/home/hiraeth/Github/epic-kitchens-100-annotations')
-    train_X, train_Y = linux_load.get_train(8)
-    print(train_X.shape)
+    windows_load = loader.Loader('C:/Users/SAM/EPIC-KITCHENS',
+                                 'C:/Users/SAM/Documents/GitHub/epic-kitchens-100-annotations',
+                                 'C:/Users/SAM/Documents/GitHub/egocentric-compressed-learning/data')
+    """ linux_load = loader.Loader('/home/hiraeth/EPIC-KITCHENS',
+                               '/home/hiraeth/Github/epic-kitchens-100-annotations',
+                               '/home/hiraeth/Github/egocentric-compressed-learning') """
+    print(windows_load.load_from_pt('train_Y.pt').size())
 
 
 if __name__ == '__main__':
