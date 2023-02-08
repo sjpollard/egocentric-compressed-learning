f923671834666cd7207fede73d9a300f9e421b25
Sam Pollard <40839788+sjpollard@users.noreply.github.com>
2023-02-08 16:55:32 +0000

    Function for creating random Bernoulli matrices.

--- compress.py
+++ compress.py
@@ -1,17 +1,23 @@
 import numpy as np
 import tensorly as tl
 import torch
+import math
 
 tl.set_backend('pytorch')
 
 
 def random_gaussian_matrix(shape):
-
+    m = shape[0]
     mu = 0.0
-    sigma = 1.0 / shape[0]
+    sigma = 1.0 / m
     M = torch.normal(mu, sigma, size=shape)
-
     return M
+
+
+def random_bernoulli_matrix(shape):
+    root_m = math.sqrt(shape[0])
+    M = np.random.default_rng().choice([-1.0 / root_m, 1.0/root_m], shape, p=[0.5, 0.5])
+    return torch.from_numpy(M).float()
 
 
 def compress_tensor(X, phi_matrices, modes):
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import tensorly as tl
 import torchvision
 import compress
 import data
-import torch
 
 tl.set_backend('pytorch')
 
@@ -22,19 +21,16 @@ def main():
     """ linux_loader = data.Loader('/home/hiraeth/EPIC-KITCHENS',
                                '/home/hiraeth/Github/epic-kitchens-100-annotations',
                                '/home/hiraeth/Github/egocentric-compressed-learning') """
-    #preprocess_epic(windows_loader)
+    # preprocess_epic(windows_loader)
     train_X, train_Y = windows_loader.load_from_pt('train_X.pt'), windows_loader.load_from_pt('train_Y.pt')
-    clip = train_X[100]
+    clip = train_X[0]
     torchvision.transforms.functional.to_pil_image(clip[3]).show()
-    W1 = compress.random_gaussian_matrix((100, 224))
-    #W1 = torch.eye(224)
-    W2 = compress.random_gaussian_matrix((100, 224))
-    #W2 = torch.eye(224)
-    compressed_clip = compress.compress_tensor(clip.float(), [W1, W2], [3, 2])
-    torchvision.transforms.functional.to_pil_image(compressed_clip[3].to(torch.uint8)).show()
-    expanded_clip = compress.expand_tensor(compressed_clip, [W1.T, W2.T], [3,2])
-    torchvision.transforms.functional.to_pil_image(expanded_clip[3].to(torch.uint8)).show()
-
+    M1 = compress.random_bernoulli_matrix((100, 224))
+    M2 = compress.random_bernoulli_matrix((100, 224))
+    compressed_clip = compress.compress_tensor(clip.float(), [M2], [2])
+    torchvision.transforms.functional.to_pil_image(compressed_clip[3]).show()
+    expanded_clip = compress.expand_tensor(compressed_clip, [M2.T], [2])
+    torchvision.transforms.functional.to_pil_image(expanded_clip[3]).show()
 
 
 if __name__ == '__main__':
