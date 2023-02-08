fb6929cbee3297395b7e1a622c0d48a1f2ba8483
Sam Pollard <40839788+sjpollard@users.noreply.github.com>
2023-02-08 16:11:13 +0000

    Demo of how to cast back to uint8 for displaying compressed image, along with identity matrix test.

--- main.py
+++ main.py
@@ -24,14 +24,16 @@ def main():
                                '/home/hiraeth/Github/egocentric-compressed-learning') """
     #preprocess_epic(windows_loader)
     train_X, train_Y = windows_loader.load_from_pt('train_X.pt'), windows_loader.load_from_pt('train_Y.pt')
-    clip = train_X[0].float()
+    clip = train_X[100]
     torchvision.transforms.functional.to_pil_image(clip[3]).show()
-    print(tl.context(clip.float()))
     W1 = compress.random_gaussian_matrix((100, 224))
+    #W1 = torch.eye(224)
     W2 = compress.random_gaussian_matrix((100, 224))
+    #W2 = torch.eye(224)
     compressed_clip = compress.compress_tensor(clip.float(), [W1, W2], [3, 2])
-    #expanded_clip = compress.expand_tensor()
-    torchvision.transforms.functional.to_pil_image(compressed_clip[3]).show()
+    torchvision.transforms.functional.to_pil_image(compressed_clip[3].to(torch.uint8)).show()
+    expanded_clip = compress.expand_tensor(compressed_clip, [W1.T, W2.T], [3,2])
+    torchvision.transforms.functional.to_pil_image(expanded_clip[3].to(torch.uint8)).show()
 
 
 
