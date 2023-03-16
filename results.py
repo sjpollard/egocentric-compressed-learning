df46485956049f87c465345282b0a034b5b97751
Sam Pollard <40839788+sjpollard@users.noreply.github.com>
2023-03-16 18:18:57 +0000

    Loops through all runs in the results project. Grabs exactly the right number of run samples.

--- results.py
+++ results.py
@@ -8,16 +8,14 @@ def main():
         os.makedirs(f'results')
 
     api = wandb.Api()
-    entity, project = 'sjpollard', 'egocentric-compressed-learning'
+    entity, project = 'sjpollard', 'egocentric-compressed-learning-results'
     runs = api.runs(entity + "/" + project)
 
-    #for run in runs: 
-    
-    name = runs[3].config['model_label']
-    history_df = runs[3].history(samples=10000)
-    run_df = history_df[['train/verb-accuracy', 'train/noun-accuracy', 'val/verb-accuracy', 'val/noun-accuracy', 'test/verb-accuracy', 'test/noun-accuracy']]
-
-    run_df.to_csv(f'results/{name}.csv', index=False)
+    for run in runs:
+        name = run.config['model_label']
+        history_df = run.history(samples=run.summary['_step'] + 1)
+        run_df = history_df[['train/verb-accuracy', 'train/noun-accuracy', 'val/verb-accuracy', 'val/noun-accuracy', 'test/verb-accuracy', 'test/noun-accuracy']]
+        run_df.to_csv(f'results/{name}.csv', index=False)
 
 if __name__ == "__main__":
     main()
