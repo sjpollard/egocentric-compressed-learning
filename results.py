6c953ed931b11319d76247132f92ecff91ab1570
Sam Pollard <40839788+sjpollard@users.noreply.github.com>
2023-03-19 23:26:16 +0000

    Plotting separate verb/noun graphs of test accuracy as spatial compression varies.

--- results.py
+++ results.py
@@ -3,6 +3,9 @@ import argparse
 import pandas as pd 
 import wandb
 import os
+import numpy as np
+
+from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser(
     description="Manages compilation and plotting of results from W&B",
@@ -33,28 +36,70 @@ def results_to_csv():
         run_df = history_df[['train/verb-accuracy', 'train/noun-accuracy', 'val/verb-accuracy', 'val/noun-accuracy', 'test/verb-accuracy', 'test/noun-accuracy']]
         run_df.to_csv(f'results/{name}.csv', index=False)
 
-def plot_accuracies():
-    return
+def plot_verb_accuracies(title, filename, measurement_rates, results_files):
+    bernoulli_test_accuracies = []
+    gaussian_test_accuracies = []
+    for results_file in results_files[0]:
+        results_df = pd.read_csv(f'results/{results_file}')
+        bernoulli_test_accuracies.append(results_df['test/verb-accuracy'].dropna().tail(3).mean())
+    for results_file in results_files[1]:
+        results_df = pd.read_csv(f'results/{results_file}')
+        gaussian_test_accuracies.append(results_df['test/verb-accuracy'].dropna().tail(3).mean())
+    plt.title(title)
+    plt.xticks(measurement_rates)
+    #plt.xlim([1, 8])
+    #plt.ylim([2, 3.4])
+    plt.plot(measurement_rates, bernoulli_test_accuracies, marker='x', label="Bernoulli")
+    plt.plot(measurement_rates, gaussian_test_accuracies, marker='x', label="Gaussian")
+    plt.legend(loc="upper left")
+    plt.xlabel("Measurement rate")
+    plt.ylabel("Accuracy(%)")
+    plt.savefig(f'images/{filename}.pdf')
+    plt.clf()
+
+def plot_noun_accuracies(title, filename, measurement_rates, results_files):
+    bernoulli_test_accuracies = []
+    gaussian_test_accuracies = []
+    for results_file in results_files[0]:
+        results_df = pd.read_csv(f'results/{results_file}')
+        bernoulli_test_accuracies.append(results_df['test/noun-accuracy'].dropna().tail(3).mean())
+    for results_file in results_files[1]:
+        results_df = pd.read_csv(f'results/{results_file}')
+        gaussian_test_accuracies.append(results_df['test/noun-accuracy'].dropna().tail(3).mean())
+    plt.title(title)
+    plt.xticks(measurement_rates)
+    #plt.xlim([1, 8])
+    #plt.ylim([2, 3.4])
+    plt.plot(measurement_rates, bernoulli_test_accuracies, marker='x', label="Bernoulli")
+    plt.plot(measurement_rates, gaussian_test_accuracies, marker='x', label="Gaussian")
+    plt.legend(loc="upper left")
+    plt.xlabel("Measurement rate")
+    plt.ylabel("Accuracy(%)")
+    plt.savefig(f'images/{filename}.pdf')
+    plt.clf()
 
 def main(args):
     if args.pull_results:
         results_to_csv()
     if args.plot_results:
-        val_accuracies = []
-        test_accuracies = []
-        results_files = ['P01_P02_20_v1.csv', 
+        spatial_results = [['P01_P02_20_v1.csv', 
                          'P01_P02_bernoulli_158_158_2_3_20_v1.csv', 
                          'P01_P02_bernoulli_112_112_2_3_20_v1.csv',
                          'P01_P02_bernoulli_71_71_2_3_20_v1.csv',
-                         'P01_P02_bernoulli_22_22_2_3_20_v1.csv']
-        for results_file in results_files:
-            results_df = pd.read_csv(f'results/{results_file}')
-            val_accuracies.append([results_df['val/verb-accuracy'].dropna().tail(3).mean(), 
-                                   results_df['val/noun-accuracy'].dropna().tail(3).mean(),])
-            test_accuracies.append([results_df['test/verb-accuracy'].dropna().tail(3).mean(), 
-                                    results_df['test/noun-accuracy'].dropna().tail(3).mean(),])
-        print(val_accuracies)
-        print(test_accuracies)
+                         'P01_P02_bernoulli_22_22_2_3_20_v1.csv'],
+                         ['P01_P02_20_v1.csv', 
+                         'P01_P02_gaussian_158_158_2_3_20_v1.csv', 
+                         'P01_P02_gaussian_112_112_2_3_20_v1.csv',
+                         'P01_P02_gaussian_71_71_2_3_20_v1.csv',
+                         'P01_P02_gaussian_22_22_2_3_20_v1.csv']]
+        plot_verb_accuracies("Verb Accuracy - Spatial Compression - Test Split",
+                        'verb_spatial_test',
+                        [1, 0.5, 0.25, 0.1, 0.01], 
+                        spatial_results)
+        plot_noun_accuracies("Noun Accuracy - Spatial Compression - Test Split",
+                        'noun_spatial_test',
+                        [1, 0.5, 0.25, 0.1, 0.01], 
+                        spatial_results)
 
 
 if __name__ == "__main__":
