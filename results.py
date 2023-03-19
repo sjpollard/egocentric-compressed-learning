56c4ce50bf05010e59156fb71b5c67c2aa589fef
Sam Pollard <40839788+sjpollard@users.noreply.github.com>
2023-03-19 20:00:07 +0000

    Average val/test accuracies across last 3 points ready for plotting.

--- results.py
+++ results.py
@@ -5,13 +5,18 @@ import wandb
 import os
 
 parser = argparse.ArgumentParser(
-    description="Hub for everything necessary to train the neural network",
+    description="Manages compilation and plotting of results from W&B",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
 parser.add_argument(
     "--pull-results", 
     action="store_true", 
-    help="Pulls results from wandb into csv"
+    help="Pulls results from W&B into csv"
+)
+parser.add_argument(
+    "--plot-results", 
+    action="store_true", 
+    help="Plots W&B results"
 )
 
 def results_to_csv():
@@ -28,9 +33,29 @@ def results_to_csv():
         run_df = history_df[['train/verb-accuracy', 'train/noun-accuracy', 'val/verb-accuracy', 'val/noun-accuracy', 'test/verb-accuracy', 'test/noun-accuracy']]
         run_df.to_csv(f'results/{name}.csv', index=False)
 
+def plot_accuracies():
+    return
+
 def main(args):
     if args.pull_results:
         results_to_csv()
+    if args.plot_results:
+        val_accuracies = []
+        test_accuracies = []
+        results_files = ['P01_P02_20_v1.csv', 
+                         'P01_P02_bernoulli_158_158_2_3_20_v1.csv', 
+                         'P01_P02_bernoulli_112_112_2_3_20_v1.csv',
+                         'P01_P02_bernoulli_71_71_2_3_20_v1.csv',
+                         'P01_P02_bernoulli_22_22_2_3_20_v1.csv']
+        for results_file in results_files:
+            results_df = pd.read_csv(f'results/{results_file}')
+            val_accuracies.append([results_df['val/verb-accuracy'].dropna().tail(3).mean(), 
+                                   results_df['val/noun-accuracy'].dropna().tail(3).mean(),])
+            test_accuracies.append([results_df['test/verb-accuracy'].dropna().tail(3).mean(), 
+                                    results_df['test/noun-accuracy'].dropna().tail(3).mean(),])
+        print(val_accuracies)
+        print(test_accuracies)
+
 
 if __name__ == "__main__":
     main(parser.parse_args())
